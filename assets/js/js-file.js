f5ec1020c1683134cd51e4424ee7145f84f8efa7
unknown <mariam@pst.ps>
2019-12-19 14:34:36 +0200

    responsive 3

--- assets/js/js-file.js
+++ assets/js/js-file.js
@@ -1,23 +1,43 @@
 $('.owl-carousel').owlCarousel({
+    autoplay:true,
+    autoplayHoverPause:true,
+    lazyLoad:true,
     loop:true,
-    items:1,
-    margin:10,
-    nav:true,
-    responsiveClass:true,
+    margin:5,
+    stagePadding:5 ,
     responsive:{
         0:{
             items:1,
-            nav:true
+            dots:false
         },
-        600:{
-            items:3,
-            nav:false
+        485:{
+            items:2,
+            dots: false
         },
-        1000:{
+        728:{
             items:3,
-            nav:true,
-            loop:false
+            dots:false
+        },
+        960:{
+            items:4,
+            dots: false
         }
-
+        ,
+        1200:{
+            items:5,
+            dots: true
+        }
     }
+
+
+});
+
+$('.owl-carousel').on('mousewheel','.owl-stage',function (e) {
+    if(e.deltaY>0){
+        $('.owl-carousel').trigger('next.owl');
+    }else{
+        $('.owl-carousel').trigger('prev.owl');
+    }
+    e.preventDefault();
+
 });
